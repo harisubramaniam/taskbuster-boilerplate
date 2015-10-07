a1477fd933eb5203843c00dae9f27afca96a62b5
Hari Subramaniam <subramh@asu.edu>
2015-10-07 11:57:54 -0700

    robots.txt, humans.txt and other enhancements

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -26,5 +26,11 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertEqual(h1.value_of_css_property("color"), 
                          "rgba(200, 50, 255, 1)")
 
+    def test_home_file(self):
+        self.browser.get(self.live_server_url + "/robots.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
