7227869e7de7b568099dce9ce3446fa404210ddf
Hari Subramaniam <subramh@asu.edu>
2015-10-08 15:12:17 -0700

    Added db

--- taskbuster/settings/development.py
+++ taskbuster/settings/development.py
@@ -3,3 +3,17 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskbuster_db',
+        'USER': 'tasker',
+        'PASSWORD': 'task3r',
+        'HOST': '',
+        'PORT': '',
+    }
+}
